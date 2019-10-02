59cb9cad7caa24294673915cd677366b1ffb4bb0
stryngs <support@configitnow.com>
2019-10-01 21:44:46 -0400

    Modified sweep() to allow for dir recreation

--- SRC/officeTasks/lib/generic.py
+++ SRC/officeTasks/lib/generic.py
@@ -3,30 +3,35 @@ import re
 import shutil
 
 class Generic(object):
-    """Things that don't have a specific "thing"""
+    """Things that don't have a specific 'thing'"""
 
-    def sweep(self, objName):
+    def sweep(self, objName, mkdir = False):
         """Lazy broom attempt for files of directories"""
         if os.path.isdir(objName):
             try:
                 shutil.rmtree(objName, ignore_errors = True)
+                success = True
             except:
                 pass
         else:
             try:
                 os.remove(objName)
+                success = True
             except:
                 pass
 
-    
+        if success is True and mkdir is True:
+            os.mkdir(objName)
+
+
     def fileMenu(self, fileType = 'csv', mDir = '.'):
         """Create a menu for input based on fileType for the current directory
-        
+
         Using this function is a nice lazy way to create a numbered menu for
         input based on filetype
-        
+
         Uses current directory as the default location
-        
+
         Returns a semi formatted menu
         """
         tgtList = []
@@ -34,7 +39,7 @@ class Generic(object):
         for i in dirList:
             if re.search('\.{0}$'.format(fileType), i):
                 tgtList.append(i)
-        
+
         fMenu = ''
         for n, i in enumerate(tgtList):
             fMenu += ('[{0}] {1}\n'.format(n, i))
