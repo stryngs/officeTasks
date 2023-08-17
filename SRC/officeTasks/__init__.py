9181cdbcd8b33064de187c97116e188bf2f50ad7
stryngs <support@configitnow.com>
2023-08-17 01:46:39 -0400

    Removed XML, but saved gist

--- SRC/officeTasks/__init__.py
+++ SRC/officeTasks/__init__.py
@@ -2,11 +2,22 @@ from .lib import compares
 from .lib import csv
 from .lib import eml
 from .lib import generic
-from .lib import xml
+
+"""
+Storing XML thoughts just because
+
+## Base
+tree = etree.parse(xml)
+root = self.tree.getroot()
+
+## Iteratives
+iterList = [i for i in self.root.iter()]
+iterSet = set(self.iterList)
+rootList = list(self.root)
+"""
 
 ## Instantiations
 cmp = compares.Compare()
 csv = csv.Csv()
 eml = eml.Eml()
 gnr = generic.Generic()
-xml = xml.Xml()
